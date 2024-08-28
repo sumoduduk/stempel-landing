5b4edf41418f8e933992e195914fc675aa279568
sumoduduk <sumoduduk13@gmail.com>
2024-08-29 02:57:09 +0700

    feat: make next js static

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.pravatar.cc",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "github.com",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
