d53c6ae1b5543d580f86214b9868d65f91581131
sumoduduk <sumoduduk13@gmail.com>
2024-08-29 13:19:55 +0700

    chore: change title layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,38 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
-import { Navbar } from "@/components/layout/navbar";
-import { ThemeProvider } from "@/components/layout/theme-provider";
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+import './globals.css';
+
+import { cn } from '@/lib/utils';
+import { Navbar } from '@/components/layout/navbar';
+import { ThemeProvider } from '@/components/layout/theme-provider';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Shadcn - Landing template",
-  description: "Landing template from Shadcn",
+    title: 'Fast Watermark - Stempel',
+    description: 'Stempel : Fast Image Watermarking App',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-br" suppressHydrationWarning>
-      <body className={cn("min-h-screen bg-background", inter.className)}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Navbar />
+    return (
+        <html lang='pt-br' suppressHydrationWarning>
+            <body className={cn('min-h-screen bg-background', inter.className)}>
+                <ThemeProvider
+                    attribute='class'
+                    defaultTheme='light'
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <Navbar />
 
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
